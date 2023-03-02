9e569635a88e28c0ef32930b34c0474858e974ca
Greatness Prodigy <49532326+thegrtnx@users.noreply.github.com>
2023-03-03 00:37:16 +0100

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './css/style.css';
 import Navbar from './nav';
 import Homepage from './home';
 import Footer from './footer';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 function App() {
   return (
